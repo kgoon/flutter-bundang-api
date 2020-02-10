63ab301a45c84a253bea33530da854420028af51
seungkil choi <seung-kil.choi@raizegls.com>
2020-02-10 12:16:16 +0900

    환경설정파일 위치 추가 작업과 관련된 문서

--- src/main/java/org/flutterstudy/api/ServletInitializer.java
+++ src/main/java/org/flutterstudy/api/ServletInitializer.java
@@ -5,6 +5,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	// `spring.config.additional-location` 설정 근거
+	// https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-external-config-application-property-files
 	private static final String ADDITIONAL_PROPERTIES =
 			"spring.config.additional-location=classpath:/secure/";
 
